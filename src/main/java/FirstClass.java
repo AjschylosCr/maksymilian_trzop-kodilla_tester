359ab3729a991f4d161763bbfc1bd4a36fd7d107
Maksymilian Trzop <maksymilian.trzop@gmail.com>
2020-06-18 23:47:37 +0200

    Initial version

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,17 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String example = "Hello from FirstClass!";
-        System.out.println(example);
+        Notebook notebook= new Notebook("600g",1000);
+        System.out.println(notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
+
+        Notebook heavyNotebook = new Notebook("2000g", 1500);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook( "1600g", 500);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        oldNotebook.checkPrice();
+
+
     }
 }
--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -1,11 +1,16 @@
 public class Loops {
-    public static void main(String[] args);
-    public int sumNumbers(int[] numbers) {
+    public static void main(String[] args) {
+        int [] tablica = {1,2,3,4};
+        sumNumbers(tablica);
+        System.out.println(sumNumbers(tablica));
+        for (int i = 0; i <= 10; i++)
+            System.out.println(i);
+        }
+    public static int sumNumbers(int[] numbers) {
         int result = 0;
         for (int i = 0; i < numbers.length; i++) {
             result = result + numbers[i];
         }
         return result;
     }
-
 }
