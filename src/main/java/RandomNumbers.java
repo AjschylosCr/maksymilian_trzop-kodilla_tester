3d12a57f8f576a97660457e26dfd281083d365f9
Maksymilian Trzop <maksymilian.trzop@gmail.com>
2020-06-19 21:57:40 +0200

    4

---
+++ src/main/java/RandomNumbers.java
@@ -0,0 +1,12 @@
+public class RandomNumbers {
+    public static void main(String[] args) {
+
+        for(int i=0; i<30; i++)
+    }
+    static int randomBetween (int start, int end) {
+        Random random = new Random();
+        int a1 = randomnextInt(end - start +1);
+        int a2 = a1 +start;
+        return a2;
+    }
+}
