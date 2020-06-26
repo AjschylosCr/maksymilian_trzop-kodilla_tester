b8f395db92c61af35f264cf0ce8b9ec81b38942b
Maksymilian Trzop <maksymilian.trzop@gmail.com>
2020-06-26 20:20:54 +0200

    fixing

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,17 +1,17 @@
-public class Book {
-
-    private String author;
-    private String tittle;
-
-
-
-    public static Book.of(String author, String tittle);{
-        this.author = author;
-        this.tittle = tittle;
-    }
-
-    public static void main (String[] args){
-        Book book = new Book();
-        Book book = Book.of(author, tittle);
-    }
-}
+//public class Book {
+//
+//    private String author;
+//    private String tittle;
+//
+//
+//
+//    public static Book.of(String author, String tittle);{
+//        this.author = author;
+//        this.tittle = tittle;
+//    }
+//
+//    public static void main (String[] args){
+//        Book book = new Book();
+//        Book book = Book.of(author, tittle);
+//    }
+//}
