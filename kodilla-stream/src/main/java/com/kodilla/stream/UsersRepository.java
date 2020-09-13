f4801c72c5359a8148dfc8655a46beed9e37c095
Maksymilian Trzop <maksymilian.trzop@gmail.com>
2020-09-13 20:22:12 +0200

    13(1).09.20

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersRepository.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersRepository.java
@@ -7,7 +7,7 @@ public class UsersRepository {
     public static List<User> getUsersLists(){
         List<User> users = new ArrayList<>();
         users.add(new User("Walter White", 50, 7, "Chemists"));
-        users.add(new User("Jessie Pinkman", 25, 4648, "Sales"));
+        users.add(new User("Jessie Pinkman", 25, 48, "Sales"));
         users.add(new User("Tuco Salamanca", 34, 116, "Manager"));
         users.add(new User("Gus Fring", 49, 0, "Board"));
         users.add(new User("Gale Boettlicher", 44, 2, "Chemists"));
--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -4,13 +4,21 @@ import com.kodilla.stream.UsersRepository;
 
 public class ForumStats {
     public static void main(String[] args) {
-        double avg = UsersRepository.getUsersLists()//list<user>
+        double avgOver40 = UsersRepository.getUsersLists()//list<user>
                 .stream()//stream<user>
                 .filter(u -> u.getAge() >= 40)//
                 .map(u -> u.getNumberOfPosts())//stream<integer>
                 .mapToInt(n ->n)//intStream
                 .average()//optionalDouble
                 .getAsDouble();
-        System.out.println(avg);
+        System.out.println(avgOver40);
+        double avgUnder40 = UsersRepository.getUsersLists()//list<user>
+                .stream()//stream<user>
+                .filter(u -> u.getAge() < 40)//
+                .map(u -> u.getNumberOfPosts())//stream<integer>
+                .mapToInt(n ->n)//intStream
+                .average()//optionalDouble
+                .getAsDouble();
+        System.out.println(avgUnder40);
     }
 }
