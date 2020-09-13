3d16d284367c5a78e05d76b257a1bebeffeb1fc1
Maksymilian Trzop <maksymilian.trzop@gmail.com>
2020-09-13 20:55:26 +0200

    13(2).09.20

--- kodilla-stream/src/main/java/com/kodilla/stream/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/com/kodilla/exception/homework/Warehouse.java
@@ -1,4 +1,22 @@
 package com.kodilla.stream.com.kodilla.exception.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Warehouse {
+    private List<Order> orderList;
+
+    public Warehouse() {
+        this.orderList = new ArrayList<>();
+    }
+
+    public void addOrder(Order order) {
+        orderList.add(order);
+    }
+
+    public Order getOrder(String number) throws OrderDoesntExistException {
+        return orderList.stream().filter(order -> order.getNumber().equals(number))
+                .findFirst().orElseThrow(()->new OrderDoesntExistException());
+                //.findFirst().orElseThrow(OrderDoesntExistException::new);
+    }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/com/kodilla/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/com/kodilla/exception/homework/WarehouseApp.java
@@ -1,4 +1,18 @@
 package com.kodilla.stream.com.kodilla.exception.homework;
 
 public class WarehouseApp {
+    public static void main(String[] args) throws OrderDoesntExistException {
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("1"));
+        warehouse.addOrder(new Order("2"));
+        warehouse.addOrder(new Order("3"));
+        warehouse.addOrder(new Order("4"));
+        warehouse.addOrder(new Order("5"));
+
+        try {
+            System.out.println(warehouse.getOrder("6"));
+        } catch(OrderDoesntExistException e){
+            System.out.println("Order does not excist");
+        }
+    }
 }
